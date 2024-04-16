eb19f3c25d32ab4bdb9bf9f92f2f1a0ba6b524e6
Lynn Elenevich <elena.elenevich@tink.com>
2024-04-16 23:50:01 +0200

    chore: implementing randomName function api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function RandomName() {
-  const name = ['James', 'Marry', 'Patrick', 'Jennifer', 'Robert', 'Linda', 'Richard', 'Lisa', 'William'];
-  return name[Math.floor(Math.random() * name.length)];
-}
+  const [name, setName] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=1')
+      .then((response) => response.json())
+      .then((data) => setName(data.results[0].name.first))
+      .catch((err) => {
+        console.log(err.message)});
+  }, []);
+
+  return (
+    'Hello, ' + name + '!'
+  )
+};
 
 export default function MyApp() {
   const [welcomeText, setNewText] = useState('Hello, world!');
-  const randomName = RandomName();
+  const halo = RandomName()
 
   return (
     <div>
       <h1> {welcomeText}</h1>
-      <button className='greenButton' onClick={() => setNewText('Hello, ' + randomName + '!') }>
+      <button className='greenButton' onClick={() => setNewText(halo) }>
         Halo </button>
     </div>
-  );
+  )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import MyApp from './App';
 
-test('loads and displays greeting', async () => {
-  render(<MyApp />); 
-  expect(screen.getByText('Hello, world!')).toBeInTheDocument();
+describe('My App', () => {
+  beforeEach(() => {
+    render(<MyApp />); 
+  })
+
+  it('loads and displays greeting', async () => {
+    expect(screen.getByText('Hello, world!')).toBeInTheDocument();
+  })
+  
+  it('has a button', () => {
+    expect(screen.getByRole('button')).toBeEnabled();
+  });
+
+  describe('Button', () => {
+    it('changes text upon clicking', () => {
+      expect(screen.getByText('Hello, world!')).toBeInTheDocument();
+    
+      screen.getByRole('button').click();
+      expect(screen.getByText(/Hello, [A-Za-z]+!/)).not.toEqual('Hello, world!');
+    });
+  })
 })
-
-test('button is present', () => {
-  render(<MyApp />);
-  expect(screen.getByRole('button')).toBeEnabled();
-});
-
-test('button changes text upon clicking', () => {
-  render(<MyApp />);
-  expect(screen.getByText('Hello, world!'));
-
-  screen.getByRole('button').click();
-  expect(screen.getByText('Hello, world!')).not.toEqual('Hello, world!');
-});
