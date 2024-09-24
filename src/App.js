52ec93ae7a2fee6675335af04279fe5520013623
Lynn Elenevich <elena.elenevich@tink.com>
2024-09-24 10:23:52 +0200

    feature: add providers page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import { useState, useCallback } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Example from './pages/Example';
+import { useState, useCallback } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Example from "./pages/Example";
+import Providers from "./pages/Providers";
 
 async function randomName() {
   try {
-    const response = await fetch('https://randomuser.me/api/?results=1');
+    const response = await fetch("https://randomuser.me/api/?results=1");
     const data = await response.json();
     return data.results[0].name.first;
   } catch (err) {
     console.log(err.message);
   }
-};
+}
 
-function MyApp({greeting = 'Hello, world!'}) {
+function MyApp({ greeting = "Hello, world!" }) {
   const [welcomeText, setNewText] = useState(greeting);
 
   const handleClick = useCallback(async () => {
-    const name = await randomName()
-    setNewText('Hello, ' + name + '!')
-  }, [])
+    const name = await randomName();
+    setNewText("Hello, " + name + "!");
+  }, []);
 
   return (
-    <div>
-      <h1> {welcomeText}</h1>
-      <button className='greenButton' onClick={handleClick}>
-        Halo </button>
+    <div className="example-page">
+      <div className="content">
+        <h1> {welcomeText}</h1>
+        <button className="greenButton" onClick={handleClick}>
+          Halo{" "}
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 const App = () => {
@@ -36,6 +40,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MyApp />} />
         <Route path="/example" element={<Example />} />
+        <Route path="/providers" element={<Providers />} />
       </Routes>
     </Router>
   );
